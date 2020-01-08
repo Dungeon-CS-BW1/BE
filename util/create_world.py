5c7372aeda9476023b34ed9639c9a43d0d9d2481
Hannah Tuttle <hannah@Hannahs-Air.PK5001Z>
2020-01-08 12:51:16 -0800

    changed csv to .py, created rooms and descriptions
    Co-authored-by: Matt Thatcher <thatmatt11@gmail.com>
    Co-authored-by: Neha <neha.r.p@gmail.com>

--- util/create_world.py
+++ util/create_world.py
@@ -4,44 +4,44 @@ from adventure.models import Player, Room
 
 Room.objects.all().delete()
 
-r_outside = Room(title="Outside Cave Entrance",
+r_1 = Room(title="Outside Cave Entrance",
                description="North of you, the cave mount beckons")
 
-r_foyer = Room(title="Foyer", description="""Dim light filters in from the south. Dusty
+r_2 = Room(title="Foyer", description="""Dim light filters in from the south. Dusty
 passages run north and east.""")
 
-r_overlook = Room(title="Grand Overlook", description="""A steep cliff appears before you, falling
+r_3 = Room(title="Grand Overlook", description="""A steep cliff appears before you, falling
 into the darkness. Ahead to the north, a light flickers in
 the distance, but there is no way across the chasm.""")
 
-r_narrow = Room(title="Narrow Passage", description="""The narrow passage bends here from west
+r_4 = Room(title="Narrow Passage", description="""The narrow passage bends here from west
 to north. The smell of gold permeates the air.""")
 
-r_treasure = Room(title="Treasure Chamber", description="""You've found the long-lost treasure
+r_5 = Room(title="Treasure Chamber", description="""You've found the long-lost treasure
 chamber! Sadly, it has already been completely emptied by
 earlier adventurers. The only exit is to the south.""")
 
-r_outside.save()
-r_foyer.save()
-r_overlook.save()
-r_narrow.save()
-r_treasure.save()
+r_1.save()
+r_2.save()
+r_3.save()
+r_4.save()
+r_5.save()
 
 # Link rooms together
-r_outside.connectRooms(r_foyer, "n")
-r_foyer.connectRooms(r_outside, "s")
+r_1.connectRooms(r_2, "n")
+r_2.connectRooms(r_1, "s")
 
-r_foyer.connectRooms(r_overlook, "n")
-r_overlook.connectRooms(r_foyer, "s")
+r_2.connectRooms(r_3, "n")
+r_3.connectRooms(r_2, "s")
 
-r_foyer.connectRooms(r_narrow, "e")
-r_narrow.connectRooms(r_foyer, "w")
+r_2.connectRooms(r_4, "e")
+r_4.connectRooms(r_2, "w")
 
-r_narrow.connectRooms(r_treasure, "n")
-r_treasure.connectRooms(r_narrow, "s")
+r_4.connectRooms(r_5, "n")
+r_5.connectRooms(r_4, "s")
 
 players=Player.objects.all()
 for p in players:
-  p.currentRoom=r_outside.id
+  p.currentRoom=r_1.id
   p.save()
 
---
+++ util/create_world_generator.py
@@ -0,0 +1,4 @@
+from adventure.rooms import rooms
+from adventure.models import Room
+
+print(rooms)
